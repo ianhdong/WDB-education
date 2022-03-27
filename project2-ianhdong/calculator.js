9456433c8638ebf1aa5e260f548950117cd8b5ab
Ian Dong <ihdong@berkeley.edu>
2022-03-27 12:44:54 -0700

    updated links

--- project2-ianhdong/calculator.js
+++ project2-ianhdong/calculator.js
@@ -7,30 +7,30 @@ let operator = null;
     Uncomment and fill in the blank spaces. */
 function calculations() {
     const intBuffer = parseInt(display); // Hint: Use parseInt to convert string to integer
-    if (operator === "+") {
+    if (operator === '+') {
         total += intBuffer;
     } else if (operator === '-') {
         total -= intBuffer;
-    } else if (operator === '*') {
+    } else if (operator === 'x') {
         total *= intBuffer;
-    } else if (operator === '/') {
+    } else if (operator === '÷') {
         total /= intBuffer;
     }
 }
 
 /*   FUNC DESCRIPTION: If user input is a number, create the function. */
 function makesNumber(value) {
-    if (display === "0") {
-        display = input.toString();
+    if (display === '0') {
+        display = value.toString();
     } else {
     /*  If display is not 0, meaning there is a previous number typed in already, what should we display on the screen?
-    Hint: How do we concatenate strings? If you are stuck, imagine typing in a "5" into the calculator, making display into "5". 
-    Then imagine typing "3" into the calculator. Now "3" is value and display is still at "5", so display will now be 53.  */
-        display = display + input.toString();
+    Hint: How do we concatenate strings? If you are stuck, imagine typing in a '5' into the calculator, making display into '5'. 
+    Then imagine typing '3' into the calculator. Now '3' is value and display is still at '5', so display will now be 53.  */
+        display = display + value.toString();
     }
 }
 
-/*  FUNC DESCRIPTION: If user input is not a number, create the function. Create the functionality for "C", "←", "=", and operators. */
+/*  FUNC DESCRIPTION: If user input is not a number, create the function. Create the functionality for 'C', '←', '=', and operators. */
 
 function makesSymbol(symbol) {
     //make functionality for symbol C
@@ -50,12 +50,19 @@ function makesSymbol(symbol) {
             display = display.substring(0, display.length - 1);
         }
     } else if (symbol === '=') { //make functionality if symbol is an operator
-        calculate();
+        calculations();
         display = total.toString();
         total = 0;
         operator = null;
     } else {
-        total = parseInt();
+        const intBuffer = parseInt(display);
+        if (total === 0) {
+            total = intBuffer;
+        } else {
+            calculations();
+        }
+        operator = symbol;
+        display = '0'; //stay consistent
     }
 }
 
@@ -63,23 +70,27 @@ function makesSymbol(symbol) {
     This is where we sense when a user clicks a certain button and send this information to our buttonClicked function. */
 function setListeners() {
 //Hint: We want to select all buttons from html and make it so that something happens when you click on the buttons! querySelectorAll might be helpful
-    let ______ = document.________(________); 
-    for (item of ________) {
+    let buttons = document.querySelectorAll('.buttons'); 
+    for (item of buttons) {
+        console.log(item);
+        item.addEventListener('click', function(event) {
+            buttonClicked(event.target.innerText);
+        })
     //Hint: addEventListener might be useful.
     //Hint: event.target.innerText might be helpful. innerText return type is a string
     }
 }
 
 //Make sure to call setListeners!!!
-________________________
+setListeners();
 
 /*  FUNC DESCRIPTION: Now we will write the function that takes care of when a button is clicked. */
 function buttonClicked(valueClicked) {
-    if (isNaN(parseInt(valueClicked))) { //NaN means "Not a Number"
-        //Hint: call a function we just created!
+    if (isNaN(parseInt(valueClicked))) { //NaN means 'Not a Number'
+        makesSymbol(valueClicked);
     } else {
-        //Hint: call a function we just created!
+        makesNumber(valueClicked);
     }
-    ______________________ = ___________;
+    document.querySelector('.result-screen').innerText = display;
 // Hint: we need to change what number appears on the screen! to change html, one listener you could use is querySelector
 }
