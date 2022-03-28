43c9f3551e3e925cb58bf9e72317bc849261c070
Ian Dong <ihdong@berkeley.edu>
2022-03-27 22:25:11 -0700

    added hw4

--- react-lecture1/my-app3/src/App.js
+++ react-lecture1/my-app3/src/App.js
@@ -1,22 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 
+import Button from './Button';
+import React, { useState, useEffect, useMemo } from 'react';
+
 function App() {
+
+  const [count, setCount] = useState(0);
+  const updateCount = () => {
+    setCount(count + 1);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Let's Count!</h1>
+        <button onClick={updateCount}>click me!</button>
+        <p>Count: {count}</p>
       </header>
     </div>
   );
